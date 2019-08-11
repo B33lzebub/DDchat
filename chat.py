f1c1649a49090823d0724626159a47d4e7cd66c5
B33lzebub <53938342+B33lzebub@users.noreply.github.com>
2019-08-11 22:10:34 +0700

    Update chat.py

--- chat.py
+++ chat.py
@@ -3,9 +3,10 @@ from tkinter import messagebox
 
 
 # - - - - - - - - SPAMMER - - - - - - - - - - -
-def loopproc():
-    log.insert(END, 'SYSTEM: Hello ' + name.get() + '!\n')
-    tk.after(10000, loopproc)
+#
+#def loopproc():
+#    log.insert(END, 'SYSTEM: Hello ' + name.get() + '!\n')
+#    tk.after(10000, loopproc)
 
 # - - - - - - - Entering MSG - - - - - - - - -
 def sendproc(event):
@@ -28,45 +29,47 @@ def destroy():
     tk.destroy()
 
 def hide():
-    print('') #Does not work
+    print('') #DOES NOT WORK
 
 def info():
     messagebox.showwarning('About', "- WTF is this?\n    This is offline chat for DeadDrops (deaddrops.com)\n    Use it for nicely and easyest chating and reading messages\n    by other users.\n\n- What the diffrent between editting .txt file?\n    Actually nothing. But it^s looks more brutal and\n    it coded on Python.\n\n- What is this based on?\n    This simple program based on Python and Tkinter (standard     GUI package)\n    All source code is open for reading and editting. THIS IS            NOT FOR COMERCIAL USE.\n\n- Is this really usefull thing?\n    No just for fun and becous i can.\n- Can i copy this program on my own DeadDrop?\n    Yes, sure. But be sure its last not modified version.\n\n - Where can i download last version of this program?  \n    In my GitHub repository. Link down below.\n\nGitHub: github.com/B33lzebub/DDchat\n\nCreated by B33lzebub")
 
 
 # - - - - - - - WINDOW SETTING - - - - - - - -
+#
 tk = Tk()
 text = StringVar()
 name = StringVar()
-name.set('User') #Default name
-text.set('Hello there!') #Default text msg
-tk.title('Offline Chat') #Window titel
+name.set('User') #Default USERNAME
+text.set('Hello there!') #Default TEXT
+tk.title('Offline Chat') #Title of window
 tk.geometry('500x600+10+10') #Window size
-tk.resizable(False,False) #Resizable offed
-tk.overrideredirect(True) #Make window look nice 
+tk.resizable(False,False) #Make window unresizable
+tk.overrideredirect(True) #Make window looks nice
 tk.tkraise()
 
 # - - - - - - - FRAMES in WINDOW - - - - - - -
-
-frame1 = Frame(tk, bg='black')
-frame2 = Frame(tk, bg='black')
-frame3 = Frame(tk, bg='black')
+#
+frame1 = Frame(tk, bg='black') #Title + 2 Buttons
+frame2 = Frame(tk, bg='black') #Main part - LOG + Scrollbar
+frame3 = Frame(tk, bg='black') #Nickname + MSG
 
 frame1.pack(side = TOP, expand='False', fill = X)
 frame2.pack(side = TOP, expand='False', fill = X)
 frame3.pack(side = BOTTOM, expand='False')
 
 # - - - - - - - OBJECTS AND APPS - - - - - - - -
-log = Text(frame2, height=29, bg='black', fg='white') #LOG of mgs
-titel = Label(frame1, text="Log Cat and All That Things", height=1, bg='black', fg='white') #Title of NICE window 
-nick = Entry(frame3, textvariable=name, borderwidth=2, bg='black', fg='white') #Nickname
-ibut = Button(frame3, text="v. 0.0.3 alpha", bg='black', fg='white') #Information button
-msg = Entry(frame3, textvariable=text, borderwidth=2, bg='black', fg='white') #MSGspace
-but = Button(frame3, text='Send', bg='yellow') #Enter button
-xbut = Button(frame1, text='╳', bg='black', fg='white', font='times 9') #Close window button
-sbut = Button(frame1, text='—', bg='black', fg='white', font='times 9') #Hide window button
-scrollbar = Scrollbar(log, orient = VERTICAL, bg='black', width='12', jump='1') #Scrollbar
-# - - - - - - - PACKs of OBJ&APPS - - - - - - - - -
+#
+scrollbar = Scrollbar(frame2, orient = VERTICAL, bg='black', width='13', jump='1') #SCROLLBAR
+log = Text(frame2, height=29, bg='black', fg='white', yscrollcommand = scrollbar.set) #LOG
+titel = Label(frame1, text="Log Cat and All That Things", height=1, bg='black', fg='white') #TITEL
+nick = Entry(frame3, textvariable=name, borderwidth=2, bg='black', fg='white') #NICKNAME
+ibut = Button(frame3, text="v. 0.0.4 alpha", bg='black', fg='white') #INFORMATION BUTTON
+msg = Entry(frame3, textvariable=text, borderwidth=2, bg='black', fg='white') #MESSAGE
+but = Button(frame3, text='Send', bg='yellow') #ENTER BUTTON
+xbut = Button(frame1, text='╳', bg='black', fg='white', font='times 9') #CLOSE BUTTON
+sbut = Button(frame1, text='—', bg='black', fg='white', font='times 9') #HIDE BUTTON
+# - - - - - - - USING OBJ&APPS - - - - - - - - -
 #
 #nick.pack(side=BOTTOM, fill='x', expand='false')
 #msg.pack(side=LEFT, fill='x', expand='true')
@@ -74,9 +77,11 @@ scrollbar = Scrollbar(log, orient = VERTICAL, bg='black', width='12', jump='1') 
 xbut.pack(side=RIGHT, expand='false', ipadx=25, ipady=1)
 sbut.pack(side=RIGHT, expand='false', ipadx=25, ipady=1)
 titel.pack(side=LEFT, fill='x', expand='false')
+scrollbar.pack(side = RIGHT, ipady=210, expand='True')
 log.pack(side=LEFT, fill='both', expand='True')
-scrollbar.pack(side = RIGHT, ipady=205)
 
+# - - - - - - - - - GRIDS - - - - - - - - - -
+#
 #Upside
 nick.grid(row=1, column=1, rowspan=1, columnspan=1, ipady=15, ipadx=135)
 ibut.grid(row=1, column=2, rowspan=1, columnspan=1, ipady=13, ipadx=14)
@@ -84,26 +89,28 @@ ibut.grid(row=1, column=2, rowspan=1, columnspan=1, ipady=13, ipadx=14)
 msg.grid(row=5, column=1, rowspan=1, columnspan=1, ipady=20, ipadx=135)
 but.grid(row=5, column=2, rowspan=1, columnspan=1, ipady=17, ipadx=35)
 
-#Configurations of OBJECTS
+# - - - - - - OBJ-S CONFIGURATIONS - - - - - - -
+#
 but.configure(command=button_clicked)
 xbut.configure(command=destroy)
 sbut.configure(command=hide)
 ibut.configure(command=info)
 scrollbar.config(command = log.yview)
-log.insert(END, '---------------------------------------------------\n  ____ _________ ____    ____ ____ ____ ____ ____\n /  _ Y  __/  _ Y  _ \  /  _ Y  __Y  _ Y  __Y ___\\\n | | \|  \ | / \| | \|  | | \|  \/| / \|  \/|    \\\n | |_/|  /_| |-|| |_/|  | |_/|    / \_/|  __|___ |\n \____|____\_/ \|____/  \____|_/\_\____|_/  \____/\n\n---------------------------------------------------\n                 _   _______   _\n                ( ) /       \ ( )\n                 \\\\/  _   _  \//\n                  \| (_) (_) |/\n                   |    _    |\n                   /\ _/_\_ /\\\n                  // |     | \\\\\n                 (_) |[] []| (_)\n                     |_____|\n\n---------------------------------------------------\n\n')
-log.insert(END, 'SUSTEM: SHOWED 50 LAST MASSAGES\n---------------------------------------------------\n')
+log.insert(END, '-'*51 + '\n  ____ _________ ____    ____ ____ ____ ____ ____\n /  _ Y  __/  _ Y  _ \  /  _ Y  __Y  _ Y  __Y ___\\\n | | \|  \ | / \| | \|  | | \|  \/| / \|  \/|    \\\n | |_/|  /_| |-|| |_/|  | |_/|    / \_/|  __|___ |\n \____|____\_/ \|____/  \____|_/\_\____|_/  \____/\n\n' + '-'*51 + '\n                 _   _______   _\n                ( ) /       \ ( )\n                 \\\\/  _   _  \//\n                  \| (_) (_) |/\n                   |    _    |\n                   /\ _/_\_ /\\\n                  // |     | \\\\\n                 (_) |[] []| (_)\n                     |_____|\n\n' + '-'*51 + '\n\n')
+log.insert(END, 'SUSTEM: SHOWED 50 LAST MASSAGES\n' + '-'*51 + '\n')
 
 # - - - - Opening and reading TAIL of DB - - - -
+#
 with open('msgs.txt') as f:
     data = f.readlines()
     tail = data[-50:]
 
-# - - - - - - Showing tail in LOG - - - - - - -
+# - - - - - - Showing tail in LOG - - - - - - - -
+#
 for line in tail:
     log.insert(END, (line))
 
 f = open('msgs.txt','a')
-
 
 msg.bind('<Return>', sendproc)
 #tk.after(10000, loopproc) - SPAMMER
