2cf9cacf33fe6e816478ef88c7211ff1fa3f8786
B33lzebub <53938342+B33lzebub@users.noreply.github.com>
2019-08-13 17:05:05 +0700

    DDchat v 0.1.0 alpha stable
    WHATS NEW:
     - New default messages
     - New method username generator
     - Redesign (as little)
     - Optimising source code

--- chat.py
+++ chat.py
@@ -1,65 +1,60 @@
+#DDchat v. 0.1.0
 from tkinter import *
 from tkinter import messagebox
-from tkinter import ttk
+import random
 
 
-def win2():
-
-    def open_win1():
-        tk.destroy()
-        win1()
-
-    tk=Tk()
-    tk.lift() #Lift window over others
-    tk.attributes('-topmost', True) #Lift window over others
-    text = StringVar()
-    text.set('Hello there!')  # Default TEXT
-    tk.title('Offline Chat')  # Title of window
-    tk.geometry('48x26+10+10')  # Window size
-    tk.resizable(False, False)  # Make window unresizable
-    tk.overrideredirect(True)  # Make window looks nice
-    tk.tkraise()
-
-    frame1 = Frame(tk, bg='black')  # Title + 2 Buttons
-    frame1.pack(side=TOP, expand='False', fill=X)
-
-    but = Button(frame1, text='Back', bg='yellow')  # ENTER BUTTON
-
-    but.pack(expand='false', ipadx=5, ipady=1)
-
-    but.configure(command=open_win1)
-
-    tk.mainloop()
-
+# - - - - - - - - - - MAIN WINDOW - - - - - - - - - - -
+#
 def win1():
+
+    # - - - - - - GENERATOR Random part of USERNAME - - - - - -
+    #
+    def secgen():
+        str = '1234567890'*10 + 'qwertyuiopasdfghjklzxcvbnm'*9 + 'QWERTYUIOPASDFGHJKLZXCVBNM'*6 + '~!@#$%^&*****:?==+<>' #Consist of
+        lst = list(str) #str -> list
+        random.shuffle(list(str)) #Mixing
+        secret = ''.join([random.choice(lst) for x in range(6)]) # Picking
+        return secret
+
+    # - - - - - - GENERATOR Random default message - - - - - - -
+    #
+    def sayhi():
+        de = ['> Enter your message here <'] #1
+        dn = ['Hello there!','Hi, wtf is going on?','pls upload cpp...','It was a virus?','GoDD luck evreyone!','What\'s new guys?'] #2,3,4,5,6
+        da = dn + de*9 #Probability of #1 = 3/5;   6/(9+6) = 6/15 = 3/5;
+        hi = ''.join([random.choice(da)]) #Picking
+        return hi
+
     # - - - - - - - - SPAMMER - - - - - - - - - - -
     #
     # def loopproc():
     #    log.insert(END, 'SYSTEM: Hello ' + name.get() + '!\n')
     #    tk.after(10000, loopproc)
+    #Only for testing and non public use, srsly. Don't be an asshole.
 
     # - - - - - - - Entering MSG - - - - - - - - -
-    def sendproc(event):
-        log.config(state=NORMAL)
-        if (text.get() and name.get()) != '':
-            log.insert(END, name.get() + ': ' + text.get() + '\n')
-            f.write(name.get() + ': ' + text.get() + '\n')
-            text.set('')
-
-        else:
-            log.insert(END, '' + 'ENTER USER NAME OR MASSAGE\n')
-        log.config(state=DISABLED)
+    #
+    def sendproc(event): #Forwarding
+        button_clicked()
 
     def button_clicked():
-        log.config(state=NORMAL)
-        if (text.get() and name.get()) != '':
-            log.insert(END, name.get() + ': ' + text.get() + '\n')
-            f.write(name.get() + ': ' + text.get() + '\n')
+        log.config(state=NORMAL) #Making log editable
+        if ((text.get() and name.get()) != '') and (text.get() != "> Enter your message here <") and (name.get() != "> Enter your username here <"):
+            log.insert(END, name.get() + ': ' + text.get() + '\n') #If message not empty and not stupid-default
+            f.write(name.get() + ': ' + text.get() + '\n') #Writing
             text.set('')
         else:
-            log.insert(END, '' + 'ENTER USER NAME OR MASSAGE\n')
-        log.config(state=DISABLED)
+            if (text.get() == '') or (text.get() == "> Enter your message here <"):
+                log.insert(END, '' + '[!] ENTER YOUR MASSAGE [!]\n')
+                text.set('> Enter your message here <')
+            else:
+                log.insert(END, '' + '[!] ENTER USERNAME [!]\n')
+                name.set('> Enter your username here <')
+        log.config(state=DISABLED) #Making log non editable
 
+    # - - - - - Closing and hiding window - - - - - - -
+    #
     def destroy():
         tk.destroy()
 
@@ -67,52 +62,55 @@ def win1():
         tk.destroy()
         win2()
 
-
+    # - - - - - - - - INFO / ABOUT - - - - - - - - -
+    #
     def info():
         messagebox.showwarning('About',
-                               "- WTF is this?\n    This is offline chat for DeadDrops (deaddrops.com)\n    Use it for nicely and easyest chating and reading messages\n    by other users.\n\n- What the diffrent between editting .txt file?\n    Actually nothing. But it^s looks more brutal and\n    it coded on Python.\n\n- What is this based on?\n    This simple program based on Python and Tkinter (standard     GUI package)\n    All source code is open for reading and editting. THIS IS            NOT FOR COMERCIAL USE.\n\n- Is this really usefull thing?\n    No just for fun and becous i can.\n- Can i copy this program on my own DeadDrop?\n    Yes, sure. But be sure its last not modified version.\n\n - Where can i download last version of this program?  \n    In my GitHub repository. Link down below.\n\nGitHub: github.com/B33lzebub/DDchat\n\nCreated by B33lzebub")
+                               "- WTF is this?\n    This is offline chat for DeadDrops (deaddrops.com)\n    Use it for nicely and easyest chating and reading messages\n    by other users.\n\n- What the diffrent between editting .txt file?\n    Actually nothing. But it^s looks more brutal and\n    it coded on Python.\n\n- What is this based on?\n    This simple program based on Python and Tkinter (standard     GUI package)\n    All source code is open for reading and editting. THIS IS            NOT FOR COMERCIAL USE.\n\n- Is this really usefull thing?\n    No just for fun and becous i can.\n\n- Can i copy this program on my own DeadDrop?\n    Yes, sure. But be sure its last not modified version.\n\n - Where can i download last version of this program?  \n    In my GitHub repository. Link down below.\n\nGitHub: github.com/B33lzebub/DDchat\n\nCreated by B33lzebub")
 
-    # - - - - - - - WINDOW SETTING - - - - - - - -
+
+    # - - - - - - - WINDOW SETTING  - - - - - - - -
     #
     tk = Tk()
     tk.lift() #Lift window over others
     tk.attributes('-topmost', True) #Lift window over others
-    text = StringVar()
-    name = StringVar()
-    name.set('User') #Default USERNAME
-    text.set('Hello there!') #Default TEXT
     tk.title('Offline Chat') #Title of window
     tk.geometry('500x605+10+10') #Window size
-    tk.resizable(False,False) #Make window unresizable
+    tk.resizable(False,False) #Make window non resizable
     tk.overrideredirect(True) #Make window looks nice
     tk.tkraise()
 
     # - - - - - - - FRAMES in WINDOW - - - - - - -
     #
     frame1 = Frame(tk, bg='black') #Title + 2 Buttons
-    frame2 = Frame(tk, bg='black') #Main part - LOG + Scrollbar
+    frame2 = Frame(tk, bg='black') #Main part: LOG + Scrollbar
     frame3 = Frame(tk, bg='black') #Nickname + MSG
 
     frame1.pack(side = TOP, expand='False', fill = X)
     frame2.pack(side = TOP, expand='False', fill = X)
     frame3.pack(side = BOTTOM, expand='False')
 
+    # - - - - - - - - - StringVar-s - - - - - - - -
+    #
+    text = StringVar()
+    name = StringVar()
+    name.set('User_' + secgen())  # Default USERNAME
+    text.set(sayhi())  # Default TEXT
+
     # - - - - - - - OBJECTS AND APPS - - - - - - - -
     #
     scrollbar = Scrollbar(frame2, orient = VERTICAL, bg='black', width='13', jump='1') #SCROLLBAR
     log = Text(frame2, height=29, bg='black', fg='white', yscrollcommand = scrollbar.set) #LOG
-    titel = Label(frame1, text="Log Cat and All That Things", height=1, bg='black', fg='white') #TITEL
+    titel = Label(frame1, text="DDchat - offline chat", height=1, bg='black', fg='white') #TITEL
     nick = Entry(frame3, textvariable=name, borderwidth=2, bg='black', fg='white') #NICKNAME
-    ibut = Button(frame3, text="v. 0.0.6 alpha", bg='black', fg='white') #INFORMATION BUTTON
+    ibut = Button(frame3, text="v. 0.1.0 alpha", bg='black', fg='white') #INFORMATION BUTTON
     msg = Entry(frame3, textvariable=text, borderwidth=2, bg='black', fg='white') #MESSAGE
     but = Button(frame3, text='Send', bg='yellow') #ENTER BUTTON
     xbut = Button(frame1, text='╳', bg='black', fg='white', font='times 9') #CLOSE BUTTON
     sbut = Button(frame1, text='—', bg='black', fg='white', font='times 9') #HIDE BUTTON
-    # - - - - - - - USING OBJ&APPS - - - - - - - - -
+
+    # - - - - - - - - - PACKING - - - - - - - - -
     #
-    #nick.pack(side=BOTTOM, fill='x', expand='false')
-    #msg.pack(side=LEFT, fill='x', expand='true')
-    #but.pack(side=LEFT, expand='false')
     xbut.pack(side=RIGHT, expand='false', ipadx=25, ipady=1)
     sbut.pack(side=RIGHT, expand='false', ipadx=25, ipady=1)
     titel.pack(side=LEFT, fill='x', expand='false')
@@ -137,8 +135,12 @@ def win1():
     scrollbar.config(command = log.yview)
     nick.config(insertbackground='white')
     msg.config(insertbackground='white')
-    log.insert(END, '-'*51 + '\n  ____ _________ ____    ____ ____ ____ ____ ____\n /  _ Y  __/  _ Y  _ \  /  _ Y  __Y  _ Y  __Y ___\\\n | | \|  \ | / \| | \|  | | \|  \/| / \|  \/|    \\\n | |_/|  /_| |-|| |_/|  | |_/|    / \_/|  __|___ |\n \____|____\_/ \|____/  \____|_/\_\____|_/  \____/\n\n' + '-'*51 + '\n                 _   _______   _\n                ( ) /       \ ( )\n                 \\\\/  _   _  \//\n                  \| (_) (_) |/\n                   |    _    |\n                   /\ _/_\_ /\\\n                  // |     | \\\\\n                 (_) |[] []| (_)\n                     |_____|\n\n' + '-'*51 + '\n\n')
-    log.insert(END, 'SUSTEM: SHOWED 50 LAST MASSAGES\n' + '-'*51 + '\n')
+
+    # - - - - - - - - SYSTEM MSGS - - - - - - - - - -
+    #
+    log.insert(END, '-'*51 + '\n  ____ _________ ____    ____ ____ ____ ____ ____\n /  _ Y  __/  _ Y  _ \  /  _ Y  __Y  _ Y  __Y ___\\\n | | \|  \ | / \| | \|  | | \|  \/| / \|  \/|    \\\n | |_/|  /_| |-|| |_/|  | |_/|    / \_/|  __|___ |\n \____|____\_/ \|____/  \____|_/\_\____|_/  \____/\n\n' + '-'*51 + '\n                 _   _______   _\n                ( ) /       \ ( )\n                 \\\\/  _   _  \//\n                  \| (_) (_) |/\n                   |    _    |\n                   /\ _/_\_ /\\\n                  // |     | \\\\\n                 (_) |[] []| (_)\n                     |_____|\n\n' + '-'*51 + '\n')
+    log.insert(END, 'DDchat: SHOWEN 50 LAST MASSAGES\n' + '-'*51 + '\n')
+    log.insert(END, '\n' + ' ' * 10 + '> DON\'T FORGET ENTER YOUR NICKNAME! <' + '\n\n')
     log.config(state=DISABLED)
 
     # - - - - Opening and reading TAIL of DB - - - -
@@ -153,13 +155,60 @@ def win1():
     for line in tail:
         log.insert(END, (line))
     log.config(state=DISABLED)
-    #- - - - - - - - - - - - - - - - - - - - - - - -
 
-
+    #- - - - - - - - - OTHER - - - - - - - - -
+    #
     f = open('msgs.txt','a')
-    msg.bind('<Return>', sendproc)
+    msg.bind('<Return>', sendproc) #Enter button -> send
     #tk.after(10000, loopproc) - SPAMMER
-    tk.mainloop()
+    tk.mainloop() #loop
 
-win1()
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+# - - - - - - - - - HIDDEN WINDOW - - - - - - - - - - - #
+#
+def win2():
 
+    # - - - - - - Opening main window - - - - -
+    #
+    def open_win1():
+        tk.destroy()
+        win1()
+
+    # - - - - - - - WINDOW SETTING  - - - - - - - -
+    #
+    tk=Tk()
+    tk.lift() #Lift window over others
+    tk.attributes('-topmost', True) #Lift window over others
+    tk.title('Offline Chat Hidden')  #Title of window
+    tk.geometry('48x26+10+10')  #Window size
+    tk.resizable(False, False) #Make window non resizable
+    tk.overrideredirect(True)  #Make window looks nice
+    tk.tkraise()
+
+    # - - - - - - - FRAMES in WINDOW - - - - - - -
+    #
+    frame1 = Frame(tk, bg='black')  #FRAME
+
+    # - - - - - - - - - PACKING - - - - - - - - - -
+    #
+    frame1.pack(side=TOP, expand='False', fill=X)
+
+    # - - - - - - - - - OBJECTS - - - - - - - - - -
+    #
+    but = Button(frame1, text='Back', bg='yellow')  #BUTTON "Back"
+
+    # - - - - - - - - - PACKING - - - - - - - - - -
+    #
+    but.pack(expand='false', ipadx=5, ipady=1) #BUTTON size
+
+    # - - - - - - - OBJ-S CONFIGURATIONS - - - - - -
+    #
+    but.configure(command=open_win1)
+
+    tk.mainloop() #loop
+
+win1() # Main window start
+
+# THE END
+# Created by B33lzebub
+# GitHub: github.com/B33lzebub
